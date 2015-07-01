570f734eb53c3d42d82f6060e3d66c514cb1e621
Matt Styles <matt@veryfizzyjelly.com>
2015-07-01 08:36:16 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import _Button from './button'
+
+/**
+ * Export standard button
+ */
+export const Button = _Button
+
+
+/**
+ * Export object of components
+ */
+export default {
+    Button: _Button
+}
